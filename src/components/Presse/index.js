7e6f7f2064522f9c3531aea2664de00ffc01f1f3
Dave <91887838+DarkKeyMaster@users.noreply.github.com>
2022-05-25 11:58:12 +0200

    update
    update

--- src/components/Presse/index.js
+++ src/components/Presse/index.js
@@ -9,6 +9,10 @@ const Presse = () => {
         <h1 className='title'>Presse</h1>
         <p className='content'>Die GaunrmaXX Gruppe informiert regelmäßig über besondere Kinoevents und Unternehmensthemen.</p>
     </div>
+    <div id='presseM'>
+
+        <h2 className='newsHead'>Hier erscheinen Bald News !</h2>
+    </div>
    </> 
   )
 }
