6d83ef3ff1c1f284cdf0034621a208471e655f4d
dominikdeisner <deisnerdominik@dv-schulen.de>
2022-06-29 10:37:09 +0200

    Signup Update

--- src/components/LoginRegisterSystem/Signup.js
+++ src/components/LoginRegisterSystem/Signup.js
@@ -1,20 +1,42 @@
 import React, {useState} from 'react';
 import './Style.css'
+import axios from "axios";
 
 function Registrieren() {
 
-    const [inputs, setInputs] = useState({});
+    const [data, setData] = useState({
+        username: '',
+        email: '',
+        password: ''
+    })
 
-    const handleChange = (event) => {
-        const name = event.target.name;
-        const value = event.target.value;
-        setInputs(values => ({values, [name]: value}));
+    const handleChange = (e) => {
+        setData({ ...data, [e.target.name]: e.target.value })
+
+        //console.log(data);
     }
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const sendData = {
+            username: data.username,
+            email: data.email,
+            password: data.password
+        }
 
-        console.log(inputs);
+        //console.log(data);
+        axios.post('http://localhost:5000/api/users/register', sendData)
+            .then(res => {
+                if (res.data.Status == 'Invalid') {
+                    alert('Invalid User');
+                }
+
+                else {
+                    alert('User created');
+                    // eslint-disable-next-line no-restricted-globals
+                    history('/signin');
+                }
+            })
     }
 
     return (
@@ -23,11 +45,11 @@ function Registrieren() {
                 <div className="login-register-system__register">
                     <h2 className="title">Registrieren</h2>
                     <form onSubmit={handleSubmit}>
-                        <input onChange={handleChange} type="text" name="username" placeholder="Benutzername" />
+                        <input onChange={handleChange} type="text" name="username" placeholder="Benutzername" required/>
                         <br></br>
-                        <input onChange={handleChange} type="email" name="email" placeholder="E-Mail" />
+                        <input onChange={handleChange} type="email" name="email" placeholder="E-Mail" required/>
                         <br></br>
-                        <input onChange={handleChange} type="password" name="password" placeholder="Passwort" />
+                        <input onChange={handleChange} type="password" name="password" placeholder="Passwort" required/>
                         <br></br>
                         <button id="registrieren" type="submit">Registrieren</button>
                     </form>
